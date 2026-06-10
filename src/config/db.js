b8583832f3861df59a1e16211894deeef232cf03
Mayank <mayank1312.raj@gmail.com>
2026-06-10 16:07:53 +0530

    updated mysql database to cloud my sql

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,6 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+console.log(process.env.MYSQL_PUBLIC_URL);
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = pool;
